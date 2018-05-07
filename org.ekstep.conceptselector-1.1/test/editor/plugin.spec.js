e3d61195965623d5a480b7abf12f14ae906641e6
Rajesh Gupta <rajesh.gupta@compassites.net>
2018-05-07 10:51:00 +0530

    Issue #1134 fix: Testcases for concept selector

---
+++ org.ekstep.conceptselector-1.1/test/editor/plugin.spec.js
@@ -0,0 +1,125 @@
+describe("Concept Selecotr:", function() {
+    var manifest, ctrl, $scope, pluginInstance;
+    var domains = [];
+    var data = { "request": { "filters": { "objectType": ["Dimension", "Domain"] } } };
+    var respconcept = {data:{"result":{"concepts":[{"identifier":"C412","parent":["literacy"],"code":"C412","keywords":["Funtoot, Misconception"],"references":"MO025, MO031\r","consumerId":"f6878ac4-e9c9-4bc4-80be-298c5a73b447","subject":"numeracy","channel":"in.ekstep","description":"Add amounts of money(in rupees and paisa)","graph_id":"domain","nodeType":"DATA_NODE","createdOn":"2016-06-15T09:43:19.795+0000","versionKey":"1496662621072","objectType":"Concept","contents":["do_10097685","do_10094168","do_10094619","do_10094124"],"appId":"dev.ekstep.in","name":"Add money","lastUpdatedOn":"2017-06-05T11:37:01.072+0000","status":"Live","node_id":69035}]}}};
+    var resp = { data:{"result":{"domains":[{"identifier":"AI","code":"AI","keywords":["Subject","AI"],"subject":"Artificial_Intelligence","consumerId":"72e54829-6402-4cf0-888e-9b30733c1b88","channel":"in.ekstep","graph_id":"domain","nodeType":"DATA_NODE","createdOn":"2018-02-28T13:17:58.507+0000","versionKey":"1519823878507","objectType":"Domain","children":["AI1","AI3","AI4","AI2"],"appId":"ekstep_portal","name":"Artificial_Intelligence","lastUpdatedOn":"2018-02-28T13:17:58.507+0000","status":"Live","node_id":31087}], "concepts":[{"identifier":"C412","parent":["literacy"],"code":"C412","keywords":["Funtoot, Misconception"],"references":"MO025, MO031\r","consumerId":"f6878ac4-e9c9-4bc4-80be-298c5a73b447","subject":"numeracy","channel":"in.ekstep","description":"Add amounts of money(in rupees and paisa)","graph_id":"domain","nodeType":"DATA_NODE","createdOn":"2016-06-15T09:43:19.795+0000","versionKey":"1496662621072","objectType":"Concept","contents":["do_10097685","do_10094168","do_10094619","do_10094124"],"appId":"dev.ekstep.in","name":"Add money","lastUpdatedOn":"2017-06-05T11:37:01.072+0000","status":"Live","node_id":69035}], "dimensions":[{"identifier":"LD3","parent":["literacy"],"code":"LD3","keywords":["Dimension"],"consumerId":"f6878ac4-e9c9-4bc4-80be-298c5a73b447","subject":"literacy","channel":"in.ekstep","description":"Basic ability to read and write a language involves recognizing the aksharas of the language. Child should be able to recognize (read) the aksharas and their sounds, as well as write the aksharas.","graph_id":"domain","nodeType":"DATA_NODE","Subject":"literacy","versionKey":"1496769636594","objectType":"Dimension","children":["do_112326333823483904143","LO37","LO39","do_112300246933831680110","do_11230024191408537619","do_112300252824281088113","do_112300301411336192127","do_112300309856591872129","do_112301509992890368191","domain_13508","do_112322803341230080143","do_112324853724790784148","do_112324915782754304156","do_112465181541261312130","do_112473654660456448135"],"appId":"dev.ekstep.in","name":"Akshara Knowledge","lastUpdatedOn":"2017-06-06T17:20:36.594+0000","status":"Live","node_id":252}]}}};
+
+    beforeAll(function(done) {
+        CollectionEditorTestFramework.init(function() {
+            manifest = org.ekstep.pluginframework.pluginManager.getPluginManifest("org.ekstep.conceptselector");
+            pluginInstance = ecEditor.instantiatePlugin("org.ekstep.conceptselector");
+            done();
+        })
+    });
+    it("Should initialize the plugin and register the event",function(done){
+        spyOn(pluginInstance, "initialize").and.callThrough();
+        pluginInstance.initialize();
+        expect(pluginInstance.initialize).toHaveBeenCalled();
+        var eventName = manifest.id + ":init";
+        var isEventPresent = ecEditor.hasEventListener(eventName);
+        expect(isEventPresent).toBe(true); 
+        expect(pluginInstance.initialize.calls.count()).toEqual(1);
+        expect(pluginInstance.initialize.calls.count()).not.toBeGreaterThan(1);
+        done();
+    });
+
+    it("Should register event and  Get domains and dimensions data", function(done) {
+        pluginInstance.dimension = {"id":"Test","name":"identifier"};
+        pluginInstance.concepts = [];
+        ecEditor.getService('search').search = jasmine.createSpy().and.callFake(function(data, cb) {
+            cb(undefined, resp);
+        });
+        spyOn(pluginInstance, "initData").and.callThrough();
+        pluginInstance.initData(pluginInstance);
+        expect(pluginInstance.initData).toHaveBeenCalled();
+        expect(pluginInstance.initData.calls.count()).toEqual(1);
+        expect(pluginInstance.initData.calls.count()).not.toBeGreaterThan(1);
+        done();         
+
+    });
+
+     it("Should register event and  should not Get domains and dimensions data", function(done) {
+        ecEditor.getService('search').search = jasmine.createSpy().and.callFake(function(data, cb) {
+            cb(true, undefined);
+        });
+        spyOn(pluginInstance, "initData").and.callThrough();
+        pluginInstance.initData(pluginInstance);
+        expect(pluginInstance.initData).toHaveBeenCalled();
+        expect(pluginInstance.initData.calls.count()).toEqual(1);
+        expect(pluginInstance.initData.calls.count()).not.toBeGreaterThan(1);
+        done();         
+
+    });
+
+    it("Should Get concepts data and push to conceptarray", function(done) {
+        pluginInstance.resp = { data:{"result":{"domains":[{"identifier":"AI","code":"AI","keywords":["Subject","AI"],"subject":"Artificial_Intelligence","consumerId":"72e54829-6402-4cf0-888e-9b30733c1b88","channel":"in.ekstep","graph_id":"domain","nodeType":"DATA_NODE","createdOn":"2018-02-28T13:17:58.507+0000","versionKey":"1519823878507","objectType":"Domain","children":["AI1","AI3","AI4","AI2"],"appId":"ekstep_portal","name":"Artificial_Intelligence","lastUpdatedOn":"2018-02-28T13:17:58.507+0000","status":"Live","node_id":31087}], "concepts":[{"identifier":"C412","parent":["literacy"],"code":"C412","keywords":["Funtoot, Misconception"],"references":"MO025, MO031\r","consumerId":"f6878ac4-e9c9-4bc4-80be-298c5a73b447","subject":"numeracy","channel":"in.ekstep","description":"Add amounts of money(in rupees and paisa)","graph_id":"domain","nodeType":"DATA_NODE","createdOn":"2016-06-15T09:43:19.795+0000","versionKey":"1496662621072","objectType":"Concept","contents":["do_10097685","do_10094168","do_10094619","do_10094124"],"appId":"dev.ekstep.in","name":"Add money","lastUpdatedOn":"2017-06-05T11:37:01.072+0000","status":"Live","node_id":69035}], "dimensions":[{"identifier":"LD3","parent":["literacy"],"code":"LD3","keywords":["Dimension"],"consumerId":"f6878ac4-e9c9-4bc4-80be-298c5a73b447","subject":"literacy","channel":"in.ekstep","description":"Basic ability to read and write a language involves recognizing the aksharas of the language. Child should be able to recognize (read) the aksharas and their sounds, as well as write the aksharas.","graph_id":"domain","nodeType":"DATA_NODE","Subject":"literacy","versionKey":"1496769636594","objectType":"Dimension","children":["do_112326333823483904143","LO37","LO39","do_112300246933831680110","do_11230024191408537619","do_112300252824281088113","do_112300301411336192127","do_112300309856591872129","do_112301509992890368191","domain_13508","do_112322803341230080143","do_112324853724790784148","do_112324915782754304156","do_112465181541261312130","do_112473654660456448135"],"appId":"dev.ekstep.in","name":"Akshara Knowledge","lastUpdatedOn":"2017-06-06T17:20:36.594+0000","status":"Live","node_id":252}]}}};
+        pluginInstance.offset = 0;
+        var data = { "request": { "filters": { "objectType": ["Concept"] }, "offset": 0, "limit": 50 } };
+        ecEditor.getService('search').search = jasmine.createSpy().and.callFake(function(resp, cb) {
+            cb(false, respconcept);
+
+        });
+        spyOn(pluginInstance, "getConcept").and.callThrough();
+        pluginInstance.getConcept(0, 500, pluginInstance, function(){});
+        expect(pluginInstance.getConcept).toHaveBeenCalled();
+        done();
+    });
+
+    it("Should open concept selector to select concepts", function(done) {
+        pluginInstance.concepts = [];
+        var event = {"type":"org.ekstep.conceptselector:init", "target":''}
+        spyOn(pluginInstance, "initConceptBrowser").and.callThrough();
+        pluginInstance.initConceptBrowser(event, data);
+        setTimeout(function() {
+            expect(pluginInstance.initConceptBrowser).toHaveBeenCalled();
+            expect(pluginInstance.initConceptBrowser.calls.count()).toEqual(1);
+            expect(pluginInstance.initConceptBrowser.calls.count()).not.toBeGreaterThan(1);
+            done();
+        }, 1000);
+        
+       
+    });
+
+    it("Should not open concept selector to select concepts", function(done) {
+        var event = {"type":"org.ekstep.conceptselector:init", "target":''}
+        spyOn(pluginInstance, "initConceptBrowser").and.callThrough();
+        pluginInstance.initConceptBrowser(event, data);
+        setTimeout(function() {
+            expect(pluginInstance.initConceptBrowser).toHaveBeenCalled();
+            done();
+        }, 1000);
+        
+    });
+
+    
+    it("Should generate telemetry event", function(done) {
+        var telemetryData = { type: 'click', subtype: 'back', target: 'backButton', id: 24234234234 };
+        spyOn(org.ekstep.contenteditor.api, 'getCurrentStage').and.returnValue({ id: 24234234234 });
+        spyOn(pluginInstance, "generateTelemetry").and.callThrough();
+        spyOn(ecEditor.getService('telemetry'), 'interact');
+        pluginInstance.generateTelemetry(telemetryData);
+        expect(ecEditor.getService('telemetry').interact).toHaveBeenCalledWith({
+            "type": telemetryData.type,
+            "subtype": telemetryData.subtype,
+            "id": telemetryData.target,
+            "pageid": jasmine.any(Number),
+            "target":{
+                "id": "",
+                "type": "plugin",
+                "ver": ""
+            },
+            "plugin":{
+                "id": 'org.ekstep.conceptselector',
+                "ver": '1.1',
+                "category": "core"
+            },
+            "ver": "3.0"
+        });
+        expect(pluginInstance.generateTelemetry).toHaveBeenCalled();
+        expect(pluginInstance.generateTelemetry.calls.count()).toEqual(1);
+        expect(pluginInstance.generateTelemetry.calls.count()).not.toBeGreaterThan(1);
+        done();
+    });
+
+});
